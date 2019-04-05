a814077015f565db2c48740809034c5e5f0ddf4b
Richard West <r.west@northeastern.edu>
2019-04-05 11:26:29 -0400

    Copied the main.py file into an examples folder

---
+++ examples/main.py
@@ -0,0 +1,10 @@
+# Python 2 compatibility
+from __future__ import print_function
+from __future__ import division
+
+# Local imports
+from pyteck.eval_model import evaluate_model
+
+evaluate_model('Tsurushima-2009.cti', 'model_species_keys.json',
+               'nheptane_data.txt', 'data', 'models', 'results',
+               'model_variant.json')
