36252ffd11047de7cc9c2fbe368aa5949e92fdb6
Richard West <r.west@northeastern.edu>
2019-04-05 11:41:36 -0400

    Example arguments now point to existing files.

--- examples/main.py
+++ examples/main.py
@@ -5,9 +5,9 @@ from __future__ import division
 # Local imports
 from pyteck.eval_model import evaluate_model
 
-evaluate_model(model_name='Tsurushima-2009.cti',
-               spec_keys_file='model_species_keys.json',
-               dataset_file='nheptane_data.txt',
+evaluate_model(model_name='h2o2.cti',
+               spec_keys_file='spec_keys.yaml',
+               dataset_file='dataset_file.txt',
                data_path='data',
                model_path='models',
                results_path='results',
