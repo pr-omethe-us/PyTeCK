c11cf834882fd6f17310632746f154f92d181cc1
Sam <vayner.s>
2021-03-31 15:24:00 -0400

    Changed simulated ignition delay value when no ignition found from 0.0 to NaN. Changed the corresponding warning message.

--- pyteck/simulation.py
+++ pyteck/simulation.py
@@ -12,6 +12,7 @@ import os
 from collections import namedtuple
 import warnings
 import numpy as np
+from numpy.core.numeric import NaN
 from datetime import datetime
 
 # Related modules
@@ -533,10 +534,10 @@ class Simulation(object):
             self.meta['simulated-ignition-delay'] = (ignition_delays[0] - time_comp) * units.second
         else:
             warnings.warn('No ignition for case ' + self.meta['id'] +
-                          ', setting value to 0.0 and continuing',
+                          ', setting value to NaN and continuing',
                           RuntimeWarning
                           )
-            self.meta['simulated-ignition-delay'] = 0.0 * units.second
+            self.meta['simulated-ignition-delay'] = NaN * units.second
 
         # TODO: detect two-stage ignition.
         self.meta['simulated-first-stage-delay'] = np.nan * units.second
